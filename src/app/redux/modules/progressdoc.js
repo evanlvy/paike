da66a7f1b3ee5a64aa6e3de38eb1b2904dd139bd
erren.liang <evan.leung@gmail.com>
2021-08-30 19:03:01 +0800

    ProgressDoc: composing model.

--- src/app/redux/modules/progressdoc.js
+++ src/app/redux/modules/progressdoc.js
@@ -10,6 +10,7 @@ export const types = {
     SEARCH_DOC_LIST: "PROGRESSDOC/SEARCH_DOC_LIST",
     FETCH_DOC_LIST: "PROGRESSDOC/FETCH_DOC_LIST",
     FETCH_DOC: "PROGRESSDOC/FETCH_DOC",
+    SET_SELECTED_DEPAETMENT: "PROGRESSDOC/SET_SELECTED_DEPAETMENT",
     SET_OPENED_DOC_ID: "PROGRESSDOC/SET_OPENED_DOC_ID",
     ADD_DOC: "PROGRESSDOC/ADD_DOC",
     DEL_DOC: "PROGRESSDOC/DEL_DOC",
@@ -29,324 +30,202 @@ export const buildGroupStageWeekId = (stage, weekIdx, degreeId, gradeId) => {
 
 // actions
 export const actions = {
-    searchDocList: (department_id, stage=0, items_per_page=0, page_id=0) => {
-      console.log(`searchDocList: dep_id: ${department_id}`);
+    searchList: (keyword, department_id=0, stage=0, items_per_page=0, page_id=0) => {
+      console.log(`searchDocList: keyword: ${keyword}, dep_id: ${department_id}`);
       return async (dispatch, getState) => {
         try {
-          if (shouldFetchGroups(stage, getState())) {
+          if (shouldSearchList(stage, getState())) {
+            console.log("shouldSearchList: return yes!");
             dispatch(appActions.startRequest());
-            const data = await progressdocApi.queryDocListByKeyword(department_id, stage, items_per_page, page_id);
+            const data = await progressdocApi.queryDocListByKeyword(keyword, department_id, items_per_page, page_id);
             dispatch(appActions.finishRequest());
-            let groups = convertGroupsToPlain(stage, data);
-            dispatch(fetchRawplanGroupsSuccess(stage, groups));
+            let groups = convertGroupsToPlain(data);
+            dispatch(searchListSuccess(keyword, department_id, groups));
           }
         } catch (error) {
           dispatch(appActions.setError(error));
         }
       }
     },
-    fetchRawplanGroups: (stage) => {
-      console.log(`fetchRawplanGroups: ids: year: ${stage}`);
+    fetchDocList: (department_id, stage=0, items_per_page=0, page_id=0) => {
+      console.log(`fetchDocList: dep_id: ${department_id}`);
       return async (dispatch, getState) => {
         try {
-          if (shouldFetchGroups(stage, getState())) {
+          if (shouldFetchList(stage, getState())) {
+            console.log("shouldFetchList: return yes!");
             dispatch(appActions.startRequest());
-            const data = await rawplanApi.queryGroups(stage);
+            const data = await progressdocApi.queryDocList(department_id, stage, items_per_page, page_id);
             dispatch(appActions.finishRequest());
-            let groups = convertGroupsToPlain(stage, data);
-            dispatch(fetchRawplanGroupsSuccess(stage, groups));
+            let groups = convertGroupsToPlain(data);
+            dispatch(fetchListSuccess(department_id, stage, groups));
           }
+          dispatch(setSelectedDepartment(department_id, stage));
         } catch (error) {
           dispatch(appActions.setError(error));
         }
       }
     },
-    fetchRawplan: (stage, weekIdx, degreeId, gradeId) => {
-      console.log(`fetchRawplan: stage: ${stage}, weekIdx: ${weekIdx}, degreeId: ${degreeId}, gradeId: ${gradeId}`);
+    fetchDoc: (id) => {
+      console.log(`fetchDoc: doc_id: ${id}`);
       return async (dispatch, getState) => {
         try {
-            const groupStageWeekId = buildGroupStageWeekId(stage, weekIdx, degreeId, gradeId);
-            if (shouldFetchRawplan(groupStageWeekId, getState())) {
-              console.log("shouldFetchRawplan: return yes!");
+            if (shouldFetchDoc(id, getState())) {
+              console.log("shouldFetchDoc: return yes!");
               dispatch(appActions.startRequest());
-              const data = await rawplanApi.queryRawplan(stage, weekIdx, degreeId, gradeId);
+              const data = await rawplanApi.queryDoc(id);
               dispatch(appActions.finishRequest());
-              let plans = convertRawplanToPlain(data);
-              dispatch(fetchRawplanSuccess(groupStageWeekId, plans));
+              dispatch(fetchDocSuccess(id, data));
             }
-            dispatch(setSelectedGroup(groupStageWeekId));
+            dispatch(setSelectedDoc(id));
         } catch (error) {
           dispatch(appActions.setError(error));
         }
       }
-    },
-    reloadRows: () => {
-      return async (dispatch, getState) => {
-        try {
-            // Get stage week info from state
-            const groupStageWeekId = getSelectedGroup(getState());
-            let item_splited = groupStageWeekId.split('_');
-            if (item_splited.length === 4) {
-              console.log("reloadRows!");
-              dispatch(appActions.startRequest());
-              const data = await rawplanApi.queryRawplan(item_splited[0], item_splited[1], item_splited[2], item_splited[3]);
-              dispatch(appActions.finishRequest());
-              let plans = convertRawplanToPlain(data);
-              dispatch(fetchRawplanSuccess(groupStageWeekId, plans));
-              // Clear changed rows, this will trigger rerender
-              dispatch(actions.clearChanges(groupStageWeekId));
-            }
-        } catch (error) {
-          dispatch(appActions.setError(error));
-        }
-      }
-    },
-    commitRow: (rowId) => {
-      console.log(`updateRow: rowId: ${rowId}`);
-      return async (dispatch, getState) => {
-        try {
-            dispatch(appActions.startRequest());
-            const data = await rawplanApi.updateRow(rowId, getRowDiff(getState(), rowId));
-            console.log("updateRow: return ok for rowId: "+data.id);
-            dispatch(appActions.finishRequest());
-            const groupStageWeekId = getSelectedGroup(getState());
-            dispatch(actions.clearChanges(groupStageWeekId));
-        } catch (error) {
-          dispatch(appActions.setError(error));
-        }
-      }
-    },
-    setRowChanged: (groupStageWeekId, rowId, colId, planItem) => ({
-      type: types.SET_ROW_CHANGED,
-      groupStageWeekId,
-      rowId,
-      colId,
-      planItem
-    }),
-    clearChanges: (groupStageWeekId) => ({
-      type: types.CLEAR_ROW_CHANGES,
-      groupStageWeekId
-    }),
-    getChangedRowIds: () => {
-      // Use thunk to call selector with State ref. In order to peek state value only.
-      return (dispatch, getState) => {
-        const state = getState();
-        return getChangedRowIds(state);
-      }
-    },
+    }
 }
 
-const shouldFetchGroups = (stage, state) => {
-    const prev_stage = state.getIn(["rawplan", "yearId"]);
-    return prev_stage !== stage;
+const shouldSearchList = (keyword, department_id, state) => {
+  const doc_list = state.getIn(["progressdoc", "searchedList", department_id+"_"+keyword]);
+  return !doc_list || doc_list.length === 0;
 }
 
-const shouldFetchRawplan = (groupStageWeekId, state) => {
-  const planList = state.getIn(["rawplan", "planRows", groupStageWeekId, 'plans']);
-  console.log("shouldFetchRawplan: "+groupStageWeekId);
-  return !planList || planList.length === 0;
+const shouldFetchList = (department_id, stage, state) => {
+  const doc_list = state.getIn(["progressdoc", "fetchedList", department_id+"_"+stage]);
+  return !doc_list || doc_list.length === 0;
 }
 
-const convertGroupsToPlain = (stage, groupsInfo) => {
-    let groupsByIds = {};
-    console.log("Got rawplan Groups: "+JSON.stringify(groupsInfo));
-    groupsInfo.forEach(groupInfo => {
-        groupsByIds[groupInfo.id] = {id: groupInfo.id, title: groupInfo.name, grade: groupInfo.grade_id, degree: groupInfo.degree_id};
-    });
-    return groupsByIds;
+const shouldFetchDoc = (doc_id, state) => {
+  const doc = state.getIn(["progressdoc", "fetchedDoc", doc_id]);
+  return !doc || doc.length === 0;
 }
 
-const fetchRawplanGroupsSuccess = (stage, groups) => {
-    return ({
-      type: types.FETCH_RAWPLAN_GROUPS,
-      stage,
-      groups
-    })
+const convertGroupsToPlain = (groupsInfo) => {
+    console.log("Got doc Groups: "+JSON.stringify(groupsInfo));
+    return {...groupsInfo};
 }
 
-const fetchRawplanSuccess = (groupStageWeekId, plans) => {
+const searchListSuccess = (keyword, department_id, groups) => {
   return ({
-    type: types.FETCH_RAWPLAN,
-    groupStageWeekId,
-    plans
+    type: types.SEARCH_LIST,
+    keyword,
+    department_id,
+    groups
   })
 }
 
-const setSelectedGroup = (groupStageWeekId) => {
+const setSelectedDepartment = (department_id, keyword) => {
   return ({
-    type: types.SET_SELECTED_GROUP,
-    groupStageWeekId
+    type: types.SET_SELECTED_SEARCH,
+    department_id,
+    keyword
   })
 }
 
-const convertRawplanToPlain = (plans) => {
-  let data_dict = {};
-  //console.log("Got rawplan rows: "+JSON.stringify(plans));
-  plans.forEach(plan_row => {
-    data_dict[plan_row.id] = parsePlan(plan_row);
-  });
-  return {plans: data_dict, update: Date.now()};
+const fetchListSuccess = (department_id, stage, groups) => {
+  return ({
+    type: types.FETCH_LIST,
+    department_id,
+    stage,
+    groups
+  })
+}
+
+const fetchDocSuccess = (id, data) => {
+  return ({
+    type: types.FETCH_DOC,
+    id,
+    data
+  })
+}
+
+const setSelectedDoc = (id) => {
+  return ({
+    type: types.SET_OPENED_DOC_ID,
+    id
+  })
+}
+
+const setSelectedDepartment = (id, stage) => {
+  return ({
+    type: types.SET_SELECTED_DEPAETMENT,
+    id,
+    stage
+  })
 }
 
 // reducers
-const groupList = (state = Immutable.fromJS({}), action) => {
-    switch (action.type) {
-      case types.FETCH_RAWPLAN_GROUPS:
-        return state.merge(action.groups);
-      default:
-        return state;
-    }
-  }
-
-const yearId = (state = Immutable.fromJS({}), action) => {
-    switch (action.type) {
-        case types.FETCH_RAWPLAN_GROUPS:
-            return state.merge({id: action.stage});
-        default:
-            return state;
-    }
-}
-
-const planRows = (state = Immutable.fromJS({}), action) => {
+const searchedList = (state = Immutable.fromJS({}), action) => {
   switch (action.type) {
-    case types.FETCH_RAWPLAN:
-      return state.merge({[action.groupStageWeekId]: action.plans});
+    case types.SEARCH_DOC_LIST:
+      return state.merge({[action.department_id+"_"+action.keyword]: action.plans});
+    case types.SET_SELECTED_SEARCH:
+      return state.merge({selected: action.department_id+"_"+action.keyword});
     default:
       return state;
   }
 }
 
-const groupStageWeekId = (state = Immutable.fromJS({}), action) => {
+const fetchedList = (state = Immutable.fromJS({}), action) => {
   switch (action.type) {
-    case types.SET_SELECTED_GROUP:
-      return state.merge({id: action.groupStageWeekId});
+    case types.FETCH_DOC_LIST:
+      return state.merge({[action.department_id+"_"+action.stage]: action.plans});
+    case types.SET_SELECTED_DEPAETMENT:
+      return state.merge({selected: action.id+"_"+action.stage});
     default:
       return state;
   }
 }
 
-const rowChanged = (state = Immutable.fromJS({}), action) => {
+const fetchedDoc = (state = Immutable.fromJS({}), action) => {
   switch (action.type) {
-    case types.SET_ROW_CHANGED:
-      //console.log("rowChanged reducer:" + JSON.stringify(state));
-      // How to change deep level state!
-      // Ref: https://stackoverflow.com/questions/36031590/right-way-to-update-state-in-redux-reducers
-      return state.mergeDeep({
-        [action.groupStageWeekId]: {
-          [action.rowId]: {
-            [action.colId]: combinePlanItem(action.planItem)
-          }
-        }
-      });
-    case types.CLEAR_ROW_CHANGES:
-      return state.remove(action.groupStageWeekId);  // Should return ImmutableJS object instead of empty obj {} directly!
+    case types.FETCH_DOC:
+      return state.merge({[""+action.id]: action.data});
+    case types.SET_OPENED_DOC_ID:
+      return state.merge({selected: action.id})
     default:
       return state;
-  }
-}
-
-const regexPlanItem = /\D{3}_\d{2}/g;
-export const parsePlan = (plan_row) => {
-  let item_dict = {};
-  //console.log("parsePlan: "+JSON.stringify(plan_row));
-  for (let key of Object.keys(plan_row)) {
-    let value = plan_row[key];
-    //console.log("parsePlan: value:"+JSON.stringify(value));
-    if (typeof value === "string") {
-      let found = key.match(regexPlanItem);
-      if (!found || found.length < 1) {
-        // non-plan keys
-        item_dict[key] = value;
-      }
-      else {
-        // compact plan keys
-        let item_splited = value.split('$');
-        if (item_splited.length === 4) {
-          item_dict[key] = {course: item_splited[0], cid: item_splited[1], teacher: item_splited[2], tid: item_splited[3]};
-        }
-      }
-    }
-    else if (typeof value === "number") {
-      // non-plan keys
-      item_dict[key] = value;
-    }
-  }
-  //let ret_val = {id: plan_row.id, begin_week: plan_row.begin_week, to: plan_row.end_week, class: plan_row.class_name, room: plan_row.classroom};
-  //return Object.assign(ret_val, item_dict);
-  return item_dict;
-}
-
-const combinePlanItem = (item) => {
-  if (typeof item === "object") {
-    // Plan item with course name/teacher name/cid/tid
-    return Object.values(item).join("$");
-  }
-  else if (!item) {
-    // In case user delete this item!
-    return "";
-  }
-  else { // String item like Classroom
-    return item;
   }
 }
 
 const reducer = combineReducers({
-  groupList,
-  yearId,
-  planRows,
-  groupStageWeekId,
-  rowChanged,
-  //planItem,
+  searchedList,
+  fetchedList,
+  fetchedDoc,
 });
 
 export default reducer;
 
 // selectors
-export const getGroups = state => state.getIn(["rawplan", "groupList"]).valueSeq();
+export const getList = state => state.getIn(["progressDoc", "fetchedList", getSelectedDepartment(state)]).valueSeq();
 
-export const getStage = (state) => state.getIn(["rawplan", "yearId", "id"]);
+export const getSearchedList = (state) => state.getIn(["progressDoc", "searchedList", getSelectedSearch(state)]).valueSeq();
 
-export const getSelectedGroup = (state) => state.getIn(["rawplan", "groupStageWeekId", "id"]);
+export const getSelectedSearch = (state) => state.getIn(["progressDoc", "searchedList", "selected"]);
 
-export const getRows = (state) => state.getIn(["rawplan", "planRows", getSelectedGroup(state), 'plans']);
+export const getSelectedDepartment = (state) => state.getIn(["progressDoc", "fetchedList", "selected"]);
 
-export const getRowDiff = (state, rowId) => state.getIn(["rawplan", "rowChanged", getSelectedGroup(state), rowId]);
+export const getSelectedDocId = (state) => state.getIn(["progressDoc", "fetchedDoc", "selected"]);
 
-export const countChangedRows = (state) => {
-  let combined_id = getSelectedGroup(state);
-  if (combined_id) {
-    if (state.hasIn(["rawplan", "rowChanged", combined_id])) {
-      // The deep in object is not immutable!
-      return Object.keys(state.getIn(["rawplan", "rowChanged", combined_id])).length;
-    }
-  }
-  return 0;
-}
+export const getDoc = (state) => state.getIn(["progressDoc", "fetchedDoc", getSelectedDocId(state)]);
 
-//export const getChangedRowIds = (state) => state.getIn(["rawplan", "rowChanged", getSelectedGroup(state)]).keySeq().toJS();
-export const getChangedRowIds = (state) => Object.keys(state.getIn(["rawplan", "rowChanged", getSelectedGroup(state)]));
-
-export const getRawplanGroups = createSelector(
-  getGroups,
-  (groups) => {
-    if (!groups) return [];
-    return groups.toJS();
+export const getDocList = createSelector(
+  getList,
+  (docList) => {
+    if (!docList) return [];
+    return docList.toJS();
   }
 );
 
-export const getPlansByGroup = createSelector(
-  getRows, 
-  (rows) => {
-    if (!rows || rows.length <= 0) {
-      return null;
-    }
-    //console.log("ReSelector: rows="+JSON.stringify(rows));
-    return Object.values(rows);
+export const getSearchedDocList = createSelector(
+  getSearchedList, 
+  (docList) => {
+    if (!docList) return [];
+    return docList.toJS();
   }
 );
 
-export const countRowChanged = createSelector(
-  countChangedRows,
+export const getDocContents = createSelector(
+  getDoc,
   (value) => {
     return value;
   }
